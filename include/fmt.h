e31ada6303956be2c2009237931ef61aa6572aac
Niz <mn@furzoom.com>
2025-03-18 22:13:15 +0800

    tmp commit fmt
    Signed-off-by: Niz <mn@furzoom.com>

---
+++ include/fmt.h
@@ -0,0 +1,25 @@
+// The Programming Language Nua.
+// Copyright (c) 2024, Nua
+// https://furzoom.com
+// https://github.com/nua-lang/nua
+//
+// MIT License
+
+#ifndef FMT_H_
+#define FMT_H_
+
+#include <stdarg.h>
+#include <utf.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct Fmt Fmt;
+struct Fmt {};
+
+#ifdef __cplusplus
+};
+#endif
+
+#endif
